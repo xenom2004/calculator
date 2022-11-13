d713d4a2e5191d5debbc5c03764422ca1ad04150
xenom2004 <117723390+xenom2004@users.noreply.github.com>
2022-11-13 13:51:48 +0530

    Updated my shitty code

--- main.cpp
+++ main.cpp
@@ -4,22 +4,14 @@
 using namespace std;
 
 
-int factorial(int number)
-{
-int max=number;
-while (max>1){
-max-=1;
-number=number*(max);
-}
 
 
 
-return number;
-}
 
 
 
-double pow(double x,int y)
+
+double pow(double x,int y)//power function
 
 {
     y+=1;
@@ -36,34 +28,194 @@ return out;
 
 
 
-double sin(double x)
-{
-    if(x>0){
-while(x>=2*3.141592653589793238){
-x=x-2*3.141592653589793238;
 
-}}
- if(x<0){}
-while(x<=-2*3.141592653589793238)
-{x=x+2*3.141592653589793238;
+
+long double factorial(int number)
+{
+
+    if (number == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return number * factorial(number - 1);
+    }
+}
+
+long double pow(long double x, int y)
+
+{
+    y += 1;
+    long double out=1;
+    while (y > 1)
+    {
+        out = out * x;
+        y -= 1;
+
+    }
+
+    return out;
+}
+
+ 
+
+double sin1(double x){
+ if (x > 0)
+    {
+
+        while (x >= 2 * 3.141592653589793238)
+        {
+            x = x - 2 * 3.141592653589793238;
+            
+        }
+
+    }
+    if (x < 0)
+    {
+
+        while (x <= -2 * 3.141592653589793238)
+        {
+            x = x + 2 * 3.141592653589793238;
+        }
+    }
+    
+
+
+
+
+
+
+    double sign = 1;
+    double total_sum = 0;
+    double n = 1;
+    while(n<31){
+        total_sum = total_sum + sign*pow(x,n)/factorial(n);
+        sign = -1*sign;
+        n = n+2;
+    }
+    return total_sum;
+}
+double cos(double x)
+{
+    int n=0;
+    double sum=0;
+    int sign =1;
+
+    while(n<31){
+
+    
+    
+    sum = sum + sign*pow(x,n)/factorial(n);
+    sign=sign*(-1);
+    n=n+2;
+
+
+    }
+return sum;
 
 
 
 }
+double cos1(double x){
+    return sin1(3.14159265359/2-x);}
+double tan1(double x){
+    
+    return sin1(x)/cos1(x);
+
+}    
+
+
+
+
+
+
+
 
 
     
-return x-pow(x,3)/factorial(3)+pow(x,5)/factorial(5)-pow(x,7)/factorial(7) +pow(x,9)/factorial(9)-pow(x,11)/factorial(11)+pow(x,13)/factorial(13)-pow(x,15)/factorial(15) +pow(x,17)/factorial(17);
-
-}
-
 
 int main()
+
 {
-    double val;
-    cout<<"Enter";
-    cin>>val;
-cout<<sin(val);
-return 0;
+    double firstnumber;
+    double secondnumber;
+    char operation;
+    
+
+int tell;
+
+cout<<"write which maths function to use,  press 1 for trignometry or press 2 for operations"<<endl;
+
+cin>>tell;
+if (tell==2){
+    
+    cout<<"enter first number."<<endl;
+
+   cin>>firstnumber;
+   cout<<"enter operators (-,+,*,/)"<<endl;
+   cin>>operation;
+   cout<<"enter second number."<<endl;
+   cin>>secondnumber;
+   if (operation =='+'){
+    cout<<"your answer is "<<firstnumber+secondnumber<<endl;
+        
+    }
+   
+ if(operation=='-'){
+    cout<<"your answer is "<<firstnumber-secondnumber<<endl;
+        
+    }
+    
+    if(operation=='*'){
+    cout<<"your answer is "<<firstnumber*secondnumber<<endl;
+        
+    }
+   
+    if(operation=='/'){
+    cout<<"your answer is "<<firstnumber/secondnumber<<endl;
+    }}
+
+
+else {
+    cout<<"select your function"<<endl<<"press 1 for sin"<<endl<<"press 2 for cos"<<endl<<"press 3 for tan"<<endl;
+    int fn;
+    cin>>fn;
+
+    cout<<"enter your number"<<endl;
+    
+
+    
+    double num;
+    cin>>num;
+
+    
+    if(fn==1){cout<<sin1(num);}
+    if(fn==2){cout<<cos1(num);}
+    if(fn==3){cout<<tan1(num);}
+
+
+
 }
+return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
